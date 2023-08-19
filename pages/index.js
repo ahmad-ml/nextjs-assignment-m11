ddb038fa6e392bea650c02df17bf0f76ca7360dc
ahmad-ml <ahmad@niftyitsolution.com>
2023-08-19 06:15:08 +0600

    Project done

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+import Footer from '../components/Footer';
+import styles from './home.module.css';
+
+
+function Home() {
+    return (
+<div className={styles.container}>
+    <h1>Welcome to the Home Page</h1>
+    <p>
+        <Link href="/blog" className={styles.link}>Go to Blog</Link>
+    </p>
+    <Footer />
+</div>
+    );
+}
+
+export default Home;
